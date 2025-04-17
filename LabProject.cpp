b283c52b0cea83ba110b2deb0c6770c27a1f398b
Ivan <146815776+IvanKovaliov@users.noreply.github.com>
2025-04-17 18:16:16 +0300

    Update LabProject.cpp

--- LabProject.cpp
+++ LabProject.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <fstream>
 #include <cmath>
+#include <cstdlib>
 
 using namespace std;
 
@@ -20,6 +21,7 @@ void lab7_sortAndSearch();
 void lab8_recursion();
 void lab9_classes();
 void setRussianLocale();
+void clearConsole();
 
 // Структура для лабораторной 4
 struct Student {
@@ -66,7 +68,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_HREDRAW | CS_VREDRAW, WndProc, 0, 0, hInstance, 
         LoadIcon(NULL, IDI_APPLICATION), LoadCursor(NULL, IDC_SIZEALL), 
         CreateSolidBrush(RGB(192, 192, 192)), NULL, L"LabWindowClass", NULL };
-    RegisterClassEx(&wc);
+    if (!RegisterClassEx(&wc)) {
+        MessageBox(NULL, L"Ошибка регистрации класса окна!", L"Ошибка", MB_OK | MB_ICONERROR);
+        return 1;
+    }
 
     // Создание меню
     HMENU hMenu = CreateMenu();
@@ -87,6 +92,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     // Создание окна
     HWND hwnd = CreateWindow(L"LabWindowClass", L"Лабораторные работы", WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, hMenu, hInstance, NULL);
+    if (!hwnd) {
+        MessageBox(NULL, L"Ошибка создания окна!", L"Ошибка", MB_OK | MB_ICONERROR);
+        return 1;
+    }
 
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
@@ -103,21 +112,56 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
     case WM_COMMAND:
         switch (LOWORD(wParam)) {
-        case ID_LAB1: lab1_arrayProcessing(); break;
-        case ID_LAB2: lab2_stringProcessing(); break;
-        case ID_LAB3: lab3_graphPlotting(hwnd); break;
-        case ID_LAB4: lab4_structProcessing(); break;
-        case ID_LAB5: lab5_fileProcessing(); break;
-        case ID_LAB6: lab6_dynamicMemory(); break;
-        case ID_LAB7: lab7_sortAndSearch(); break;
-        case ID_LAB8: lab8_recursion(); break;
-        case ID_LAB9: lab9_classes(); break;
-        case ID_EXIT: DestroyWindow(hwnd); break;
+        case ID_LAB1:
+            clearConsole();
+            lab1_arrayProcessing();
+            break;
+        case ID_LAB2:
+            clearConsole();
+            lab2_stringProcessing();
+            break;
+        case ID_LAB3:
+            clearConsole();
+            lab3_graphPlotting(hwnd);
+            break;
+        case ID_LAB4:
+            clearConsole();
+            lab4_structProcessing();
+            break;
+        case ID_LAB5:
+            clearConsole();
+            lab5_fileProcessing();
+            break;
+        case ID_LAB6:
+            clearConsole();
+            lab6_dynamicMemory();
+            break;
+        case ID_LAB7:
+            clearConsole();
+            lab7_sortAndSearch();
+            break;
+        case ID_LAB8:
+            clearConsole();
+            lab8_recursion();
+            break;
+        case ID_LAB9:
+            clearConsole();
+            lab9_classes();
+            break;
+        case ID_EXIT:
+            DestroyWindow(hwnd);
+            break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
         }
         break;
     case WM_PAINT: {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
+        // Очистка окна (светло-серый фон)
+        RECT rect;
+        GetClientRect(hwnd, &rect);
+        FillRect(hdc, &rect, CreateSolidBrush(RGB(192, 192, 192)));
         EndPaint(hwnd, &ps);
         }
         break;
@@ -135,26 +179,34 @@ void setRussianLocale() {
     SetConsoleOutputCP(1251);
 }
 
+void clearConsole() {
+    system("cls"); // Очистка консоли для чистоты интерфейса
+}
+
 void lab1_arrayProcessing() {
     cout << "Лабораторная работа 1: Обработка массивов\n";
     int n;
     cout << "Введите размер массива: ";
-    cin >> n;
-    if (n <= 0) {
-        cout << "Некорректный размер!\n";
-        return;
+    while (!(cin >> n) || n <= 0) {
+        cout << "Ошибка! Введите положительное число: ";
+        cin.clear();
+        cin.ignore(10000, '\n');
     }
     vector<int> arr(n);
-    cout << "Введите элементы массива:\n";
+    cout << "Введите " << n << " элементов массива:\n";
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+        while (!(cin >> arr[i])) {
+            cout << "Ошибка! Введите целое число: ";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
     }
     sort(arr.begin(), arr.end());
     cout << "Отсортированный массив:\n";
     for (vector<int>::iterator it = arr.begin(); it != arr.end(); ++it) {
         cout << *it << " ";
     }
-    cout << "\nНажмите Enter для возврата в меню...\n";
+    cout << "\n\nЗадание выполнено. Нажмите Enter для возврата в меню...\n";
     cin.ignore();
     cin.get();
 }
@@ -165,6 +217,12 @@ void lab2_stringProcessing() {
     cout << "Введите строку: ";
     cin.ignore();
     getline(cin, str);
+    if (str.empty()) {
+        cout << "Ошибка! Строка не может быть пустой.\n";
+        cout << "Нажмите Enter для возврата в меню...\n";
+        cin.get();
+        return;
+    }
     int wordCount = 0;
     bool inWord = false;
     for (unsigned int i = 0; i < str.length(); ++i) {
@@ -176,12 +234,16 @@ void lab2_stringProcessing() {
         }
     }
     cout << "Количество слов в строке: " << wordCount << "\n";
-    cout << "Нажмите Enter для возврата в меню...\n";
+    cout << "\nЗадание выполнено. Нажмите Enter для возврата в меню...\n";
     cin.get();
 }
 
 void lab3_graphPlotting(HWND hwnd) {
     cout << "Лабораторная работа 3: Построение графика f(x) = 30*sin(5-3x)\n";
+    // Очистка окна перед рисованием
+    InvalidateRect(hwnd, NULL, TRUE);
+    UpdateWindow(hwnd);
+
     HDC hdc = GetDC(hwnd);
     HPEN hPen = CreatePen(PS_SOLID, 1, RGB(0, 0, 255));
     SelectObject(hdc, hPen);
@@ -191,11 +253,13 @@ void lab3_graphPlotting(HWND hwnd) {
     double scaleX = 50.0;
     double scaleY = 50.0;
 
+    // Оси координат
     MoveToEx(hdc, 0, centerY, NULL);
     LineTo(hdc, WINDOW_WIDTH, centerY);
     MoveToEx(hdc, centerX, 0, NULL);
     LineTo(hdc, centerX, WINDOW_HEIGHT);
 
+    // Построение графика
     for (int px = 0; px < WINDOW_WIDTH; px++) {
         double x = (px - centerX) / scaleX;
         double y = 30 * sin(5 - 3 * x);
@@ -209,7 +273,7 @@ void lab3_graphPlotting(HWND hwnd) {
 
     DeleteObject(hPen);
     ReleaseDC(hwnd, hdc);
-    cout << "График построен. Нажмите Enter для возврата в меню...\n";
+    cout << "График построен.\n\nЗадание выполнено. Нажмите Enter для возврата в меню...\n";
     cin.ignore();
     cin.get();
 }
@@ -219,28 +283,42 @@ void lab4_structProcessing() {
     vector<Student> students;
     int n;
     cout << "Введите количество студентов: ";
-    cin >> n;
-    if (n <= 0) {
-        cout << "Некорректное количество!\n";
-        return;
+    while (!(cin >> n) || n <= 0) {
+        cout << "Ошибка! Введите положительное число: ";
+        cin.clear();
+        cin.ignore(10000, '\n');
     }
     cin.ignore();
     for (int i = 0; i < n; i++) {
         Student s;
-        cout << "Имя студента: ";
+        cout << "Студент " << i + 1 << ":\n";
+        cout << "Имя: ";
         getline(cin, s.name);
+        if (s.name.empty()) {
+            cout << "Ошибка! Имя не может быть пустым.\n";
+            i--;
+            continue;
+        }
         cout << "Группа: ";
-        cin >> s.group;
+        while (!(cin >> s.group) || s.group <= 0) {
+            cout << "Ошибка! Введите положительное число: ";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
         cout << "Средний балл: ";
-        cin >> s.averageScore;
+        while (!(cin >> s.averageScore) || s.averageScore < 0 || s.averageScore > 100) {
+            cout << "Ошибка! Введите число от 0 до 100: ";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
         cin.ignore();
         students.push_back(s);
     }
-    cout << "Список студентов:\n";
+    cout << "\nСписок студентов:\n";
     for (vector<Student>::iterator it = students.begin(); it != students.end(); ++it) {
         cout << "Имя: " << it->name << ", Группа: " << it->group << ", Средний балл: " << it->averageScore << "\n";
     }
-    cout << "Нажмите Enter для возврата в меню...\n";
+    cout << "\nЗадание выполнено. Нажмите Enter для возврата в меню...\n";
     cin.get();
 }
 
@@ -248,7 +326,10 @@ void lab5_fileProcessing() {
     cout << "Лабораторная работа 5: Работа с файлами\n";
     ofstream outFile("output.txt");
     if (!outFile) {
-        cout << "Ошибка создания файла!\n";
+        cout << "Ошибка создания файла output.txt!\n";
+        cout << "Нажмите Enter для возврата в меню...\n";
+        cin.ignore();
+        cin.get();
         return;
     }
     outFile << "Пример записи в файл.\n";
@@ -256,16 +337,19 @@ void lab5_fileProcessing() {
     
     ifstream inFile("output.txt");
     if (!inFile) {
-        cout << "Ошибка чтения файла!\n";
+        cout << "Ошибка чтения файла output.txt!\n";
+        cout << "Нажмите Enter для возврата в меню...\n";
+        cin.ignore();
+        cin.get();
         return;
     }
     string line;
-    cout << "Содержимое файла:\n";
+    cout << "Содержимое файла output.txt:\n";
     while (getline(inFile, line)) {
         cout << line << "\n";
     }
     inFile.close();
-    cout << "Нажмите Enter для возврата в меню...\n";
+    cout << "\nЗадание выполнено. Нажмите Enter для возврата в меню...\n";
     cin.ignore();
     cin.get();
 }
@@ -273,11 +357,17 @@ void lab5_fileProcessing() {
 void lab6_dynamicMemory() {
     cout << "Лабораторная работа 6: Динамическая память\n";
     int rows, cols;
-    cout << "Введите количество строк и столбцов матрицы: ";
-    cin >> rows >> cols;
-    if (rows <= 0 || cols <= 0) {
-        cout << "Некорректные размеры!\n";
-        return;
+    cout << "Введите количество строк матрицы: ";
+    while (!(cin >> rows) || rows <= 0) {
+        cout << "Ошибка! Введите положительное число: ";
+        cin.clear();
+        cin.ignore(10000, '\n');
+    }
+    cout << "Введите количество столбцов матрицы: ";
+    while (!(cin >> cols) || cols <= 0) {
+        cout << "Ошибка! Введите положительное число: ";
+        cin.clear();
+        cin.ignore(10000, '\n');
     }
     int** matrix = new int*[rows];
     for (int i = 0; i < rows; i++) {
@@ -286,10 +376,10 @@ void lab6_dynamicMemory() {
             matrix[i][j] = rand() % 100;
         }
     }
-    cout << "Случайная матрица:\n";
+    cout << "\nСлучайная матрица:\n";
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            cout << matrix[i][j] << " ";
+            cout << matrix[i][j] << "\t";
         }
         cout << "\n";
     }
@@ -297,7 +387,7 @@ void lab6_dynamicMemory() {
         delete[] matrix[i];
     }
     delete[] matrix;
-    cout << "Нажмите Enter для возврата в меню...\n";
+    cout << "\nЗадание выполнено. Нажмите Enter для возврата в меню...\n";
     cin.ignore();
     cin.get();
 }
@@ -306,47 +396,55 @@ void lab7_sortAndSearch() {
     cout << "Лабораторная работа 7: Сортировка и поиск\n";
     int n;
     cout << "Введите размер массива: ";
-    cin >> n;
-    if (n <= 0) {
-        cout << "Некорректный размер!\n";
-        return;
+    while (!(cin >> n) || n <= 0) {
+        cout << "Ошибка! Введите положительное число: ";
+        cin.clear();
+        cin.ignore(10000, '\n');
     }
     vector<int> arr(n);
-    cout << "Введите элементы массива:\n";
+    cout << "Введите " << n << " элементов массива:\n";
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+        while (!(cin >> arr[i])) {
+            cout << "Ошибка! Введите целое число: ";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
     }
     sort(arr.begin(), arr.end());
-    cout << "Отсортированный массив:\n";
+    cout << "\nОтсортированный массив:\n";
     for (vector<int>::iterator it = arr.begin(); it != arr.end(); ++it) {
         cout << *it << " ";
     }
-    cout << "\nВведите число для поиска: ";
+    cout << "\n\nВведите число для поиска: ";
     int target;
-    cin >> target;
+    while (!(cin >> target)) {
+        cout << "Ошибка! Введите целое число: ";
+        cin.clear();
+        cin.ignore(10000, '\n');
+    }
     vector<int>::iterator it = lower_bound(arr.begin(), arr.end(), target);
     if (it != arr.end() && *it == target) {
         cout << "Число найдено на позиции: " << (it - arr.begin()) << "\n";
     } else {
         cout << "Число не найдено.\n";
     }
-    cout << "Нажмите Enter для возврата в меню...\n";
+    cout << "\nЗадание выполнено. Нажмите Enter для возврата в меню...\n";
     cin.ignore();
     cin.get();
 }
 
 void lab8_recursion() {
     cout << "Лабораторная работа 8: Рекурсия\n";
-    cout << "Введите число для вычисления факториала: ";
     int n;
-    cin >> n;
-    if (n < 0) {
-        cout << "Факториал не определен для отрицательных чисел!\n";
-        return;
+    cout << "Введите число для вычисления факториала: ";
+    while (!(cin >> n) || n < 0) {
+        cout << "Ошибка! Введите неотрицательное число: ";
+        cin.clear();
+        cin.ignore(10000, '\n');
     }
     long long factorial(int n);
     cout << "Факториал " << n << " = " << factorial(n) << "\n";
-    cout << "Нажмите Enter для возврата в меню...\n";
+    cout << "\nЗадание выполнено. Нажмите Enter для возврата в меню...\n";
     cin.ignore();
     cin.get();
 }
@@ -361,28 +459,43 @@ void lab9_classes() {
     vector<Book> books;
     int n;
     cout << "Введите количество книг: ";
-    cin >> n;
-    if (n <= 0) {
-        cout << "Некорректное количество!\n";
-        return;
+    while (!(cin >> n) || n <= 0) {
+        cout << "Ошибка! Введите положительное число: ";
+        cin.clear();
+        cin.ignore(10000, '\n');
     }
     cin.ignore();
     for (int i = 0; i < n; i++) {
         string title, author;
         int year;
-        cout << "Название книги: ";
+        cout << "Книга " << i + 1 << ":\n";
+        cout << "Название: ";
         getline(cin, title);
+        if (title.empty()) {
+            cout << "Ошибка! Название не может быть пустым.\n";
+            i--;
+            continue;
+        }
         cout << "Автор: ";
         getline(cin, author);
+        if (author.empty()) {
+            cout << "Ошибка! Автор не может быть пустым.\n";
+            i--;
+            continue;
+        }
         cout << "Год издания: ";
-        cin >> year;
+        while (!(cin >> year) || year < 0) {
+            cout << "Ошибка! Введите неотрицательное число: ";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
         cin.ignore();
         books.push_back(Book(title, author, year));
     }
-    cout << "Список книг:\n";
+    cout << "\nСписок книг:\n";
     for (vector<Book>::iterator it = books.begin(); it != books.end(); ++it) {
         it->display();
     }
-    cout << "Нажмите Enter для возврата в меню...\n";
+    cout << "\nЗадание выполнено. Нажмите Enter для возврата в меню...\n";
     cin.get();
 }
