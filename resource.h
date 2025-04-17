1cd252c5b58c782169ae4bb0dc20095cd1d1bfa9
Ivan <146815776+IvanKovaliov@users.noreply.github.com>
2025-04-17 17:48:56 +0300

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,13 @@
+#define IDR_MENU1 101
+#define ID_LAB1 40001
+#define ID_LAB2 40002
+#define ID_LAB3 40003
+#define ID_LAB4 40004
+#define ID_LAB5 40005
+#define ID_LAB6 40006
+#define ID_LAB7 40007
+#define ID_LAB8 40008
+#define ID_LAB9 40009
+#define ID_EXIT 40010
+
+#endif // Завершающая директива
